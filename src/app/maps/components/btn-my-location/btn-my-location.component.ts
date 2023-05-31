0bdbc09abbfaf4220b39b1eb35c16e0f17823a20
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-05-31 00:13:35 -0500

    boton flotante y logo de angular

---
+++ src/app/maps/components/btn-my-location/btn-my-location.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-btn-my-location',
+  templateUrl: './btn-my-location.component.html',
+  styleUrls: ['./btn-my-location.component.css'],
+})
+export class BtnMyLocationComponent {
+  goToMyLocation() {
+    console.log('ir a mi location');
+  }
+}
