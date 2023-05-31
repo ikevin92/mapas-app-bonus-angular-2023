9f28344ac9d33e90b8840ababf5d7a19344c8f4e
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-05-31 12:26:26 -0500

    dibujar polyline

--- src/app/maps/services/map.service.ts
+++ src/app/maps/services/map.service.ts
@@ -1,5 +1,12 @@
 import { Injectable, inject } from '@angular/core';
-import { LngLatBounds, LngLatLike, Map, Marker, Popup } from 'mapbox-gl';
+import {
+  AnySourceData,
+  LngLatBounds,
+  LngLatLike,
+  Map,
+  Marker,
+  Popup,
+} from 'mapbox-gl';
 import { Feature } from '../interfaces/places.interface';
 import { DirectionsApiClient } from '../api';
 import { DirectionsResponse, Route } from '../interfaces/directions.interface';
@@ -87,6 +94,46 @@ export class MapService {
 
     this.map?.fitBounds(bounds, {
       padding: 200,
+    });
+
+    // Polyline
+    const sourceData: AnySourceData = {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: [
+          {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'LineString',
+              coordinates: coords,
+            },
+          },
+        ],
+      },
+    };
+
+    // limpiar ruta previa
+    if (this.map.getLayer('RouteString')) {
+      this.map.removeLayer('RouteString');
+      this.map.removeSource('RouteString');
+    }
+
+    this.map.addSource('RouteString', sourceData);
+
+    this.map.addLayer({
+      id: 'RouteString',
+      type: 'line',
+      source: 'RouteString',
+      layout: {
+        'line-cap': 'round',
+        'line-join': 'round',
+      },
+      paint: {
+        'line-color': 'black',
+        'line-width': 3,
+      },
     });
   }
 }
