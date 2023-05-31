9a110377047579868004c44d53cab75558176e77
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-05-30 23:39:33 -0500

    obtener geolocalizacion del usuario

--- src/app/maps/screens/map-screen/map-screen.component.ts
+++ src/app/maps/screens/map-screen/map-screen.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { PlacesService } from '../../services';
 
 @Component({
   selector: 'app-map-screen',
   templateUrl: './map-screen.component.html',
-  styleUrls: ['./map-screen.component.css']
+  styleUrls: ['./map-screen.component.css'],
 })
-export class MapScreenComponent {
+export class MapScreenComponent implements OnInit {
+  private placesService = inject(PlacesService);
 
+  ngOnInit(): void {}
 }
