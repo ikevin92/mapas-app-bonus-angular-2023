ad6c4810359ea1d076cca6401dc547951ab26218
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-05-31 00:38:53 -0500

    debounce manual

--- src/app/maps/components/search-bar/search-bar.component.ts
+++ src/app/maps/components/search-bar/search-bar.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css']
+  styleUrls: ['./search-bar.component.css'],
 })
 export class SearchBarComponent {
+  private debounceTimer?: NodeJS.Timeout;
 
+  onQueryChange(query: string = '') {
+    if (this.debounceTimer) clearTimeout(this.debounceTimer);
+
+    this.debounceTimer = setTimeout(() => {
+      console.log('mandar este query', query);
+    }, 350);
+  }
 }
